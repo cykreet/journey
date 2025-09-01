ce33caf392620139980dbc1e08eadd0fa7f89a13
cykreet <cykreet@gmail.com>
2025-09-01 19:59:55 +0200

    fix: remove loading state from sidebar item

--- src/components/layout/menu/sidebar-item.tsx
+++ src/components/layout/menu/sidebar-item.tsx
@@ -5,29 +5,22 @@ export function SidebarItem({
 	icon,
 	active,
 	children,
-	loading,
 }: {
 	icon?: ForwardRefExoticComponent<SVGProps<SVGSVGElement>>;
 	active?: boolean;
 	disabled?: boolean;
-	loading?: boolean;
 	children?: ReactNode;
 }) {
 	const Icon = icon;
 	const classNames = clsx(
 		"w-full hover:border-goo hover:bg-goo/10 border-1 border-transparent py-1.5 flex px-3 flex-inline text-wood-100 text-sm items-center gap-2 select-none rounded-md cursor-pointer",
 		active && "bg-goo/10 text-goo!",
-		loading && "animate-pulse h-6 bg-ivory/8",
 	);
 
 	return (
-		<div className={classNames} style={{ width: loading ? `${Math.floor(Math.random() * 100)}%` : "auto" }}>
-			{!loading && (
-				<>
-					{Icon && <Icon />}
-					<span className="text-ellipsis overflow-hidden whitespace-nowrap">{children}</span>
-				</>
-			)}
+		<div className={classNames}>
+			{Icon && <Icon />}
+			<span className="text-ellipsis overflow-hidden whitespace-nowrap">{children}</span>
 		</div>
 	);
 }
